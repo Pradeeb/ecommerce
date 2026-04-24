4fff93232dd69f8d9a7cafdbdbc73ef28339a199
pradeep <valanpradeep6@gmail.com>
2026-04-24 15:26:42 +0530

    add all

--- Backend/product-service/src/test/java/com/bookstore/product/contoller/ProductController.java
+++ Backend/product-service/src/test/java/com/bookstore/product/contoller/ProductController.java
@@ -1,12 +1,9 @@
 package com.bookstore.product.contoller;
 
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
